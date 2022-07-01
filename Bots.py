b09c93bd2379d4028b2d86f001096aefd254567e
Ethical Anshul <103323453+AnshulMurnal@users.noreply.github.com>
2022-07-01 18:31:37 +0530

    Add files via upload

---
+++ Bots.py
@@ -0,0 +1,8 @@
+from POOPs import Robot
+Bot_1 = Robot("He there how are doing, Do you need any help?", "My function is two help the user", "50 kw", "Helps within seconds")
+print(Bot_1.Greetings)
+print(Bot_1.Function)
+print(Bot_1.Range)
+print(Bot_1.Efficiency)
+Bot_1.start()
+Bot_1.shutdown()
---
+++ POOPs.py
@@ -0,0 +1,13 @@
+
+class Robot:
+
+    def __init__(self, Greetings, Function, Range, Efficiency):
+        self.Greetings = Greetings
+        self.Function = Function
+        self.Range = Range
+        self.Efficiency = Efficiency
+    def start(self,):
+        print("Hello!Please wait the boot is loading it may take few seconds.....")
+    def shutdown(self):
+        print("I am off now so good bye and have great day...")
+
